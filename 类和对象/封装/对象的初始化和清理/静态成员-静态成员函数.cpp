91ef6752a0666b1178862b9750945375a2c1436e
Sebastian-Ma-67 <Sebastian_xq_ma@163.com>
2020-06-27 21:25:03 +0800

    静态成员函数

--- 类和对象/封装/对象的初始化和清理/静态成员-静态成员函数.cpp
+++ 类和对象/封装/对象的初始化和清理/静态成员-静态成员函数.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class Person
+{
+public:
+    static void func()
+    {
+        cout << "static func call" << endl;
+    }
+};
+
+void test01()
+{
+    Person p;
+    p.func();
+
+    Person::func();
+}
+
 int main()
 {
+    test01();
 
-    system("pause");
     return 0;
 }
