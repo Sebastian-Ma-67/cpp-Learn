7c13adb850961a2fe37954b2e0923d58a1c72f5b
Sebastian-Ma-67 <Sebastian_xq_ma@163.com>
2020-08-09 18:42:58 +0800

    写文件

--- 5 文件操作/5.1 文本文件/写文件.cpp
+++ 5 文件操作/5.1 文本文件/写文件.cpp
@@ -1,13 +1,20 @@
+#include <iostream>
 #include <fstream>
 using namespace std;
 
 void test01()
 {
     ofstream ofs;
-    ofs.open("‪E:\\cpp\\test01.txt", ios::out);
+    ofs.open("test01.txt", ios::out);
+    if (!ofs.is_open())
+    {
+        cout << "file open failed" << endl;
+        return;
+    }
     ofs << "name: zhangsan" << endl;
     ofs << "gender: male" << endl;
     ofs << "age: 18" << endl;
+    cout << "file is closing" << endl;
     ofs.close();
 }
 int main()
